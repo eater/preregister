5a62bb25754257c5b38ed261cd673405184765ee
eater <lists@pote.com>
2017-08-23 23:59:26 -0400

    Update metadata.php

--- conf/metadata.php
+++ conf/metadata.php
@@ -3,3 +3,4 @@
 $meta['captcha']  = array('multichoice','_choices' => array('builtin','captcha plugin','none'));
 $meta['send_confirm'] = array('onoff');
 $meta['list_age'] = array('numeric');
+$meta['allowed_domains'] = array('string');
